12a0f5a9efbdbe20b5b4124eed313cb9a27c2952
Flying-Makapaka <1186977762@qq.com>
2024-03-02 15:10:36 +0800

    秦九昭法实现二进制转十进制(*^▽^*)

--- main.cpp
+++ main.cpp
@@ -1,26 +1,12 @@
 #include <iostream>
 using namespace std;
 
-double _abs(double n){
-    if(n<0){
-        n*=-1.0;
-    }
-    return n;
-}
- 
 int main(void){
-    int a=609222657,b=389387265,l=15,min_x=1,min_y=1;
-    cin>>a>>b>>l;
-    double r=(double)b/a;
-    for(int i=1;i<=l;i++){
-        for(int j=i*r-2;j<=i*r+2;j++){
-            if(_abs(r-(double)j/i)<_abs(r-(double)min_x/min_y)&&(double)j/i<=r){
-                min_x=j;
-                min_y=i;
-            }
-        }
+    string s="1001";
+    int dec=0;
+    for(int i=0;i<s.length();i++){
+        dec=2*dec+(s[i]-'0');
     }
-    cout<<min_y<<" "<<min_x;
-    if(0)cout<<"\n"<<_abs((double)9/14-r)<<"  "<<_abs((double)7/11-r)<<endl;
+    cout<<dec<<endl;
     return 0;
 } 
