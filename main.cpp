de78594cbd2850d548a42325c58b13478c234558
Flying-Makapaka <1186977762@qq.com>
2024-03-03 10:45:18 +0800

    4917-数据结构全排列例题。老实说，并没有把dfs吃透

--- main.cpp
+++ main.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
 using namespace std;
 
-int main(void){
-    string s="1001";
-    int dec=0;
-    for(int i=0;i<s.length();i++){
-        dec=2*dec+(s[i]-'0');
+int a[20],b[20]={0};
+int n;
+
+void dfs(int k){
+    if(k==n+1){
+        for(int i=1;i<=n;i++){
+            cout<<a[i]<<" ";
+        }
+        cout<<"\n";
+        return;
     }
-    cout<<dec<<endl;
+    for(int i=1;i<=n;i++){
+        if(b[i]==0){
+            a[k]=i;
+            b[i]=1;
+            dfs(k+1);
+            b[i]=0;
+        }
+    }
+}
+
+int main(void){
+    cin>>n;
+    dfs(1);
     return 0;
 } 
