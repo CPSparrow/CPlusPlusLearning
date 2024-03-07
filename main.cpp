6ae98a8554ee2ddf09ddedabe3e4928d6f4827a3
Flying-Makapaka <1186977762@qq.com>
2024-03-07 11:11:14 +0800

    链表的简单实现。有空自己写一遍

--- main.cpp
+++ main.cpp
@@ -2,19 +2,63 @@
 #include <algorithm>
 using namespace std;
 
-int main(void){
-    int n,t,max=0,thisMax=0;
-    cin>>n;
+struct Node{
+    int data;
+    struct Node *Next; 
+};
+typedef struct Node
+        node;
+typedef node*
+        NodePtr;
+
+//chen
+NodePtr creatList(int *num,int n){
+    NodePtr head,p,q;
+    head = new Node();
+    q=head;
     for(int i=0;i<n;i++){
-        cin>>t;
-        thisMax+=t;
-        if(thisMax>max){
-            max=thisMax;
-        }
-        if(thisMax<0){
-            thisMax=0;
-        }
+        p=new node();
+        p->data=num[i];
+        q->Next=p;
+        q=p;
     }
-    cout<<max;
+    q->Next=NULL;
+    return head;
+}
+
+//chen
+NodePtr insertList(NodePtr head,int x){
+    NodePtr q,p,s;
+    s=new node();
+    s->data=x,s->Next=NULL;
+    q=head,p->Next=NULL;
+    while (p!=NULL) {
+        if(p->data>=x){
+            break;
+        }
+        q=p;
+        p=p->Next;
+    }
+    q->Next=s;
+    s->Next=p;
+    return head;
+}
+
+//chen
+void printList(NodePtr head){
+    //node_list with empty node
+    NodePtr p=head->Next;
+    while (p!=NULL) {
+        cout<<p->data<<"\n";
+        p=p->Next;
+    }
+    return;
+}
+
+int main(void) {
+    int n=5;
+    int num[n]={1,2,3,4,5};
+    NodePtr p=creatList(num, n);
+    printList(p);
     return 0;
 }
