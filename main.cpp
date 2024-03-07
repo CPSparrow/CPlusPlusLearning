b6a9ccb1bdaa2b4b67e39e524bb02d13b75a9a72
Flying-Makapaka <1186977762@qq.com>
2024-03-07 09:28:28 +0800

    数塔，一个经典DP问题（这个其实可以理解为递推）

--- main.cpp
+++ main.cpp
@@ -2,21 +2,66 @@
 #include <algorithm>
 using namespace std;
 
+void f(void);
+
 int main(void){
-    int a,b,t=0,sum=0;
-    cin>>a>>b; 
-    for(int i=a;i<=b;i++){
-        sum+=i;
-        t++;
-        printf("%5d",i);
-        if(t%5==0){
-        	t=0;
-            cout<<"\n";
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        f();
+    }
+    return 0;
+}
+
+
+///example
+/*
+5
+7
+3 8
+8 1 0 
+2 7 4 4
+4 5 2 6 5
+*/
+
+int _max(int a,int b){
+    if(a>b)return a;
+    else return b;
+}
+
+void f(void){
+    int n;
+    cin>>n;
+    int num[n][n];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i+1;j++){
+            cin>>num[i][j];
         }
     }
-    if(t!=0){
-    	cout<<"\n";
-	}
-    cout<<"Sum = "<<sum<<flush;
-    return 0;
+    num[1][0]+=num[0][0];
+    num[1][1]+=num[0][0];
+    for(int i=2;i<n;i++){
+        num[i][0]+=num[i-1][0];
+        for(int j=1;j<=i-1;j++){
+            num[i][j]+=_max(num[i-1][j],num[i-1][j-1]);
+        }
+        num[i][i]+=num[i-1][i-1];
+
+        /*//print process
+        cout<<"time = "<<i<<endl;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i+1;j++){
+                cout<<num[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+        cout<<endl;*/
+    }
+    int max=0;
+    for(int i=0;i<n;i++){
+        if(num[n-1][i]>max){
+            max=num[n-1][i];
+        }
+    }
+    cout<<max<<endl;
 }
