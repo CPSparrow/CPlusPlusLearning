cfc1e5dc987b6977b4f7f7c56a23407c69bd7118
Flying-Makapaka <1186977762@qq.com>
2024-03-13 22:16:12 +0800

    没写出来的单链表。暂存一下

--- main.cpp
+++ main.cpp
@@ -1,80 +1,119 @@
 #include<iostream>
-#include<string>
+#include<algorithm>
 using namespace std;
-
+  
 struct Node{
-    int id;
+    int num;
+    int count;
     struct Node* next;
 };
-
 typedef struct Node
-        node;
-
+    node;
 typedef node*
-        nodePtr;
+    NodePtr;
+  
+int _count=0;
+  
+//print from head to tail
+void printList(NodePtr head){
+    NodePtr p=head;
+    while(p!=NULL){
+        cout<<p->num<<" ";
+        p=p->next;
+    }
+    cout<<flush;
+}
+  
+//add node to head
+NodePtr addNode(NodePtr head,int n){
+    _count++;
+    NodePtr p=new node();
+    p->num=n;
+    p->next=head;
+    p->count=_count;
+    return p;
+}
+  
+//delete the i-th added node
+NodePtr deleteNode(NodePtr head,int n){
+    if(n==0||head->count==n){
+        head=head->next;
+        return head;
+    }
+    NodePtr p=head->next,q=head;
+    while(p->count!=n){
+        p=p->next;
+        q=q->next;
+    }
+    q->next=p->next;
+    return head;
+}
+  
+//insert a node after the i-th added node
+NodePtr insertNode(NodePtr head,int n,int num){
+    NodePtr p=head;
+    while(p->count!=n){
+        p=p->next;
+    }
+    NodePtr s=new node();
+    _count++;
+    s->num=num;
+    s->count=_count;
+    s->next=p->next;
+    p->next=s;
+    return head;
+}
+  
+/*
+H 9
+I 1 1
+D 1
+D 0
+H 6
+*/
 
-//create a reversing node list based on a list
-nodePtr createList(int *num, int n){
-    nodePtr head,p,q;
-    for(int i=0;i<n;i++){
-        p=new node();
-        p->id=num[i];
-        if(i==0){
-            head=p;
-            p->next=p;
-            q=p;
-        }else{
-            q->next=p;
-            q=p;
-            p->next=head;
-        }
+NodePtr f(NodePtr head){
+    char s;
+    int n,m;
+    cin>>s;
+    switch(s){
+        case 'H':
+            cin>>n;
+            head=addNode(head,n);
+            break;
+        case 'I':
+            cin>>n>>m;
+            head=insertNode(head,n,m);
+            break;
+        case 'D':
+            cin>>n;
+            head=deleteNode(head,n);
+            break;
+        default:
+            break;
     }
     return head;
 }
-
-//print a reversing node list
-void printList(nodePtr head){
-    nodePtr p=head;
-    do{
-        cout<<p->id<<" ";
-        p=p->next;
-    }while(p!=head);
-}
-
-//pop ever m-th node
-void popNode(nodePtr head,int m){
-    nodePtr p=head->next,q=head;
-    int count=1;
-    do{
-        count++;
-        if(count==m){
-            cout<<p->id<<" ";
-            p=p->next;
-            q->next=p;
-            //q=p;
-            count=1;
-        }
-        p=p->next;
-        q=q->next;
-    }while(p!=p->next);
-    cout<<p->id;
-}
-
-/*
-求解约瑟夫（Joseph）问题。
-有n个小孩围成一圈，给他们从1开始依次编号，从编号为1的小孩开始报数，数到第m个小孩出列，然后从出列的下一个小孩重新开始报数，数到第m个小孩又出列，…，如此反复直到所有的小孩全部出列为止，求整个出列序列。
-如当n=6，m=5时的出列序列是5，4，6，2，3，1。
-*/
-int main()
-{
-    //1 2 3 4 5 6
-    int n,m;
-    cin>>n>>m;
-    int t[n];
-    for(int i=0;i<n;i++){
-        t[i]=i+1;
-    }
-    nodePtr head = createList(t,n);
-    popNode(head, m);
+    
+/*int main(void){
+    NodePtr p = NULL;
+    p=addNode(p,9);
+    p=addNode(p,8);
+    p=addNode(p,7);
+    p=deleteNode(p,0);
+    p=addNode(p,6);
+    printList(p);
+    //cout<<"\n"<<_count<<flush;
     return 0;
-}
+}*/
+
+int main(void){
+    int n;
+    NodePtr p = NULL;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        p = f(p);
+    }
+    printList(p);
+    return 0;
+}/**/
