6b588a602fe02fed5d72431337c90d80292ddeb0
Flying-Makapaka <1186977762@qq.com>
2024-03-13 22:00:47 +0800

    循环链表。磕磕绊绊地写完了。难点在popNode（）。题干在main前

--- main.cpp
+++ main.cpp
@@ -2,50 +2,79 @@
 #include<string>
 using namespace std;
 
-const int N=210;
-int t,e[N],ne[N]={-1},head=-1,idx=0;
+struct Node{
+    int id;
+    struct Node* next;
+};
 
-void addToHead(int x){
-    e[idx]=x;
-    ne[idx]=head;
-    head=idx;
-    idx++;
+typedef struct Node
+        node;
+
+typedef node*
+        nodePtr;
+
+//create a reversing node list based on a list
+nodePtr createList(int *num, int n){
+    nodePtr head,p,q;
+    for(int i=0;i<n;i++){
+        p=new node();
+        p->id=num[i];
+        if(i==0){
+            head=p;
+            p->next=p;
+            q=p;
+        }else{
+            q->next=p;
+            q=p;
+            p->next=head;
+        }
+    }
+    return head;
 }
 
-void add(int k,int x){
-    e[idx]=x;
-    ne[idx]=ne[k];
-    ne[k]=idx;
-    idx++;
+//print a reversing node list
+void printList(nodePtr head){
+    nodePtr p=head;
+    do{
+        cout<<p->id<<" ";
+        p=p->next;
+    }while(p!=head);
 }
 
-void deleteNode(int k){
-    ne[k]=ne[ne[k]];
+//pop ever m-th node
+void popNode(nodePtr head,int m){
+    nodePtr p=head->next,q=head;
+    int count=1;
+    do{
+        count++;
+        if(count==m){
+            cout<<p->id<<" ";
+            p=p->next;
+            q->next=p;
+            //q=p;
+            count=1;
+        }
+        p=p->next;
+        q=q->next;
+    }while(p!=p->next);
+    cout<<p->id;
 }
 
+/*
+求解约瑟夫（Joseph）问题。
+有n个小孩围成一圈，给他们从1开始依次编号，从编号为1的小孩开始报数，数到第m个小孩出列，然后从出列的下一个小孩重新开始报数，数到第m个小孩又出列，…，如此反复直到所有的小孩全部出列为止，求整个出列序列。
+如当n=6，m=5时的出列序列是5，4，6，2，3，1。
+*/
 int main()
 {
-    cin>>t;
-    int x;
-    for(int i=0;i<10;i++){
-        cin>>x;
-        if(i==0){
-            addToHead(x);
-        }else{
-            add(i-1,x);
-        }
+    //1 2 3 4 5 6
+    int n,m;
+    cin>>n>>m;
+    int t[n];
+    for(int i=0;i<n;i++){
+        t[i]=i+1;
     }
-    if(t==1){
-        head=ne[head];
-    }else{
-        deleteNode(t-2);
-    }
-    for(int i=head;i!=-1;i=ne[i]){
-        if(i==head){
-            cout<<e[i];
-        }else{
-            cout<<" "<<e[i];
-        }
-    }
+    nodePtr head = createList(t,n);
+    popNode(head, m);
     return 0;
 }
