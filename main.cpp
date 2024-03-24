96df2fd0b4f2175d99fe7a7163c3dac508a5bce7
Flying-Makapaka <1186977762@qq.com>
2024-03-24 22:08:45 +0800

    增加了c++教程，抄写体验了课本上的链表逆转。md的快捷方式是 ctrl+shift+v

--- main.cpp
+++ main.cpp
@@ -6,104 +6,57 @@ typedef struct Node
     node;
 typedef node*
     nodePtr;
-
-//双向链表的定义
 struct Node{
     int num;
     nodePtr next;
-    nodePtr previous;
+    Node(int _num=0,nodePtr _next=NULL):num(_num),next(_next){}
 };
 
-//在双向链表中把node t 插入到node p 之后
-void insertNode(nodePtr p,nodePtr t){
-    t->previous=p;
-    t->next=p->next;
-    p->next->previous=t;
-    p->next=t;
+//print from head to tail
+void printList(nodePtr head){
+    nodePtr p=head;
+    while(p!=NULL){
+        cout<<p->num<<" ";
+        p=p->next;
+    }
+    cout<<flush;
 }
+ 
+nodePtr nodeReverse(nodePtr head);
 
-nodePtr createList(nodePtr head,int n){
-    nodePtr p;
+nodePtr createList(int n){
+    nodePtr head=NULL,p;
     for(int i=0;i<n;i++){
-        nodePtr s=new node();
-        s->num=i+1;
+        nodePtr s=new node(i+1,NULL);
         if(i==0){
             head=s;
-            s->next=s->previous=s;
             p=s;
         }else{
-            s->next=head;
-            s->previous=p;
             p->next=s;
-            head->previous=s;
             p=s;
         }
     }
     return head;
 }
 
-//example: 1 2 3 4 5 6, m=2
-//pop: 2 4 6 3 1 5
-nodePtr popForward(nodePtr p,int step){
-    int count=1;
-    while(p!=p->next){
-        count++;
-        p=p->next;
-        if(count==step){
-            cout<<p->num<<"*";
-            p->previous->next=p->next;
-            p->next->previous=p->previous;
-            p=p->next;
-            return p;
-        }
-    }
-    cout<<p->num;
-    return NULL;
-}
-
-//example: 1 2 3 4 5 6, m=2
-//pop: 6 4 2 5 1 3
-nodePtr popBackward(nodePtr p,int step){
-    int count=1;
-    while(p!=p->previous){
-        count++;
-        p=p->previous;
-        if(count==step){
-            cout<<p->num<<"*";
-            p->previous->next=p->next;
-            p->next->previous=p->previous;
-            p=p->previous;
-			return p;
-        }
-        
-    }
-    cout<<p->num;
-    return NULL;
-}
-
-//for debug
-void printList(nodePtr head){
-    nodePtr p=head;
-    do{
-        cout<<p->num<<"*";
-        p=p->next;
-    }while(p!=head);
-}
-
 int main(void){
-    int n=6,m=3,k=5;
-    cin>>n>>m>>k;
-    nodePtr head=NULL;
-    head=createList(head, n);
-    nodePtr p=head;
-    int tag=1;
-    while(p!=NULL){
-        if(tag==1){
-            p=popBackward(p,m);
-        }else{
-            p=popForward(p,k);
-        }
-        tag*=-1;
-    }
+    int n=5;
+    nodePtr head=createList(n);
+    head=nodeReverse(head);
+    head=nodeReverse(head);
+    printList(head);
     return 0;
 }/**/
+
+nodePtr nodeReverse(nodePtr head){
+    nodePtr oldHead,newHead,temp;
+    oldHead=head;
+    newHead=NULL;
+    while(oldHead!=NULL){
+         temp=oldHead->next;
+         oldHead->next=newHead;
+         newHead=oldHead;
+         oldHead=temp;
+    }
+    return newHead;
+}
