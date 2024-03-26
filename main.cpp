e5d3fbe91e160c26371f15f64d81cd9d3689d276
Flying-Makapaka <1186977762@qq.com>
2024-03-26 16:38:14 +0800

    一个比较简单的数组循环左移的代码。随后加上更复杂的版本

--- main.cpp
+++ main.cpp
@@ -1,65 +1,30 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
-typedef struct Node node;
-typedef node *nodePtr;
-struct Node {
-    int num;
-    nodePtr next;
-    Node(int _num = 0, nodePtr _next = NULL) : num(_num), next(_next)
-    {}
-};
-
-// print from head to tail
-void printList(nodePtr head)
-{
-    nodePtr p = head;
-    while (p != NULL) {
-        cout << p->num << " ";
-        p = p->next;
-    }
-    cout << flush;
-}
-
-nodePtr nodeReverse(nodePtr head);
-
-nodePtr createList(int n)
-{
-    nodePtr head = NULL, p;
-    for (int i = 0; i < n; i++) {
-        nodePtr s = new node(i + 1, NULL);
-        if (i == 0) {
-            head = s;
-            p = s;
-        } else {
-            p->next = s;
-            p = s;
-        }
-    }
-    return head;
-}
+void listMove(int *num, int size);
 
 int main(void)
 {
-    int n = 5;
-    nodePtr head = createList(n);
-    head = nodeReverse(head);
-    head = nodeReverse(head);
-    printList(head);
-    return 0;
-} /**/
-
-nodePtr nodeReverse(nodePtr head)
-{
-    nodePtr oldHead, newHead, temp;
-    oldHead = head;
-    newHead = NULL;
-    while (oldHead != NULL) {
-        temp = oldHead->next;
-        oldHead->next = newHead;
-        newHead = oldHead;
-        oldHead = temp;
+    int n = 5, m;
+    std::cin >> n >> m;
+    int num[n];
+    for (int i = 0; i < n; i++)
+        std::cin >> num[i];
+    for (int i = 0; i < m; i++) {
+        listMove(num, n);
     }
-    return newHead;
+    for (int i = 0; i < n - 1; i++) {
+        std::cout << num[i] << " ";
+    }
+    std::cout << num[n - 1];
+    return 0;
+} 
+
+void listMove(int *num, int size)
+{
+    int t = num[0];
+    for (int i = 0; i < size; i++) {
+        num[i] = num[i + 1];
+    }
+    num[size - 1] = t;
 }
