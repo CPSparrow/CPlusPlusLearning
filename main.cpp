80e8bb30a0473071fe0f00372af88bd12d904aa8
Flying-Makapaka <1186977762@qq.com>
2024-03-30 15:41:20 +0800

    括号匹配。但是PTA上段错误……

--- main.cpp
+++ main.cpp
@@ -1,69 +1,52 @@
 #include <iostream>
 #include <stack>
-#include <queue>
 using namespace std;
 
-stack<double> d_st;
-stack<char> op_st;
-
-int level(char a)
-{
-    if (a == '+' || a == '-') {
-        return 1;
-    }
-    if (a == '*' || a == '/') {
-        return 2;
-    }
-}
-
-void cal(void)
-{
-    double a, b;
-    char c = op_st.top();
-    op_st.pop();
-    a = d_st.top();
-    d_st.pop();
-    b = d_st.top();
-    d_st.pop();
-    switch (c) {
-        case '+':
-            d_st.push(b + a);
-            break;
-        case '-':
-            d_st.push(b - a);
-            break;
-        case '*':
-            d_st.push(b * a);
-            break;
-        case '/':
-            d_st.push(b / a);
-            break;
-        default:
-            break;
-    }
-}
+// example for(int i=0; i<v; i++){ visited[i] = 0; for(int j=0; j<v; j++) scanf("%d",&(g->Adj[i][j])); }
 
 int main(void)
 {
-    string str;
-    double x;
-    getline(cin, str);
-    int len = str.size();
-    for (int i = 0; i < len; i++) {
-        if (str[i] >= '0' && str[i] <= '9') {
-            x = str[i] - '0';
-            while (str[i+1]>='0'&&str[i+1]<='9') {
-                x = x * 10 + str[i + 1] - '0';
-                i++;
+    stack<char> op;
+    string s;
+    int left = 0, right = 0;
+    bool flag = true;
+    getline(cin, s);
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == '(' || s[i] == '[' || s[i] == '{') {
+            left++;
+            op.push(s[i]);
+        } else if (s[i] == ')') {
+            right++;
+            if (op.top() == '(') {
+                op.pop();
+            } else {
+                flag = false;
             }
-            d_st.push(x);
-        } else if (str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/') {
-            if (op_st.empty() || level(str[i]) > level(op_st.top())) {
-                op_st.push(str[i]);
-            } else if (!op_st.empty() && level(str[i]) <= level(op_st.top())) {
-                //
+        } else if (s[i] == ']') {
+            right++;
+            if (op.top() == '[') {
+                op.pop();
+            } else {
+                flag = false;
+            }
+        } else if (s[i] == '}') {
+            right++;
+            if (op.top() == '{') {
+                op.pop();
+            } else {
+                flag = false;
             }
         }
     }
+    if (op.empty() != true) {
+        flag = false;
+    }
+    cout << left << " " << right << "\n";
+    if (flag == true) {
+        cout << "YES";
+    } else {
+        cout << "NO";
+    }
+    cout << endl;
     return 0;
 }
